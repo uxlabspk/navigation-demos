8d3b07cccce9c67805533589ab2719f3c60cc6f4
Muhammad Naveed <naveed@codehuntspk.com>
2025-11-29 10:09:49 +0500

    feat: Remove ConstraintLayout and Coroutines examples
    Removes the `ConstraintLayout.kt` and `Coroutines.kt` files which contained temporary examples.
    
    This change also removes the `constraintlayout-compose` dependency and cleans up unused imports in `MainActivity` and styling in `HomeScreen`.

--- app/src/main/java/io/github/uxlabspk/navigationdemo/MainActivity.kt
+++ app/src/main/java/io/github/uxlabspk/navigationdemo/MainActivity.kt
@@ -4,21 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import io.github.uxlabspk.navigationdemo.ui.navigation.Navigation
 import io.github.uxlabspk.navigationdemo.ui.theme.NavigationDemoTheme
@@ -28,8 +15,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val navController = rememberNavController()
             NavigationDemoTheme {
-
+                Navigation(navController)
             }
         }
     }
@@ -43,7 +31,7 @@ class MainActivity : ComponentActivity() {
     heightDp = 700
 )
 @Composable
-fun GreetingPreview() {
+fun NavigationPreview() {
     val navController = rememberNavController()
     NavigationDemoTheme {
         Navigation(navController)
--- app/src/main/java/io/github/uxlabspk/navigationdemo/ui/screens/HomeScreen.kt
+++ app/src/main/java/io/github/uxlabspk/navigationdemo/ui/screens/HomeScreen.kt
@@ -1,11 +1,16 @@
 package io.github.uxlabspk.navigationdemo.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 
@@ -13,7 +18,13 @@ import androidx.navigation.compose.rememberNavController
 fun HomeScreen(
     navController: NavHostController
 ) {
-    Column() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
         Text(text = "Home Screen")
         Button(
             onClick = { navController.navigate("profile") }
